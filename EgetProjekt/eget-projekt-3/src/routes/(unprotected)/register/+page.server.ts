e37ca27d09e0e66e41941e0d9cfff4a36a590e55
calleeee <94118178+calleeee@users.noreply.github.com>
2023-06-01 13:56:44 +0200

    register done

---
+++ EgetProjekt/eget-projekt-3/src/routes/(unprotected)/register/+page.server.ts
@@ -0,0 +1,57 @@
+import { fail, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { database } from '$lib/db/db'
+import bcrypt from 'bcrypt'
+
+export const load = (async () => {
+    return {};
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+    register: async ({request, locals, cookies}) => {
+        const form = await request.formData()
+        const email = form.get('email')?.toString()
+        const password = form.get("password")?.toString()
+        const username = form.get("username")?.toString()
+        const passwordConfirm = form.get("passwordConfirm")?.toString()
+
+        let profilePic = 'https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+
+        if(
+            typeof email != 'string' ||
+            typeof username != 'string' ||
+            typeof password != 'string' ||
+            typeof passwordConfirm != 'string' ||
+            !password ||
+            !username ||
+            !passwordConfirm ||
+            !email
+        ) {
+            return fail(400, {invalid: true})
+        }
+
+        const user = await database.user.findUnique({
+            where: {username}
+        })
+
+        if(user){
+            return fail(400, {user: true})
+        }
+
+        if(password != passwordConfirm){
+            return fail(400, {match: false})
+        }
+
+        await database.user.create({
+            data: {
+                username,
+                passwordHash: await bcrypt.hash(password, 10),
+                email,
+                profilePic,
+                colortheme: 'Grey',
+                darkMode: true,
+                siteVisits: 1,
+            }
+        })
+    }
+}
---
+++ EgetProjekt/eget-projekt-3/src/routes/(unprotected)/register/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+    import type { ActionData, PageData } from './$types';
+    
+    export let form: ActionData;
+</script>
+
+<form action="?/register" use:enhance method="POST">
+    <input type="text" id="email" placeholder="email">
+
+    <input type="text" id='username' placeholder="Username">
+
+    <input type="password" id='password' placeholder="Password">
+
+    <input type="password" id="passwordConfirm" placeholder="Password Conifrm">
+
+    {#if form?.error}
+        <p class="error">{form?.error}</p>
+    {/if}
+
+    <button>Register</button>
+</form>
+
+<p>If you already have an account, <a href="/login">log in</a></p>
+
+<style>
+    .error{
+        color: tomato;
+    }
+</style>
